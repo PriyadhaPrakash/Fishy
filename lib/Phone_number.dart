78b281a899889ef449d2d7cef79ad47d88c9b6b3
Saniya Benny <218709042+saniya-benny@users.noreply.github.com.>
2025-09-27 12:33:47 +0530

    updated phone page
    Conflicts:

--- lib/Phone_number.dart
+++ lib/Phone_number.dart
@@ -12,43 +12,57 @@ class _PhoneNumberState extends State<PhoneNumber> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF151F24),
       appBar: AppBar(
+        backgroundColor: const Color(0xFF151F24),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () {},
         ),
       ),
       body: Column(
-        children: [
+        children: [SizedBox(height: 90,),
+          Text('WELCOME BACK',style: TextStyle(color: Colors.white,fontSize: 22,fontWeight: FontWeight.bold),),
           Padding(
-            padding: const EdgeInsets.only(top: 90, left: 5),
+            padding: const EdgeInsets.only(top: 30, left: 5),
             child: Text(
-              "Enter Your Phone Number",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              "Enter Your Phone Number to sign in",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white70,),
             ),
           ),
           SizedBox(height: 30.0),
-          TextField(
-            controller: phoneController,
-            keyboardType: TextInputType.phone,
-            decoration: InputDecoration(
-              hintText: "phone-number",
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12),
+          Center(
+            child: SizedBox(
+              width: 320,
+              child: TextField(
+                controller: phoneController,
+                keyboardType: TextInputType.phone,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: "phone-number",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
               ),
             ),
-          ),   const SizedBox(height: 40),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.green.shade900, // button color
-              foregroundColor: Colors.white, // text/icon color
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.zero,
-                //borderRadius: BorderRadius.zero, //Rectangular border
+          ),   const SizedBox(height: 30),
+          SizedBox(
+            width: 320,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF18A2FE),
+                minimumSize: Size(double.infinity, 48),// button color
+                foregroundColor: Colors.white, // text/icon color
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  //borderRadius: BorderRadius.zero, //Rectangular border
+                ),
               ),
+              onPressed: () {},
+              child: const Text('Continue'),
+
             ),
-            onPressed: () {},
-            child: const Text('Send OTP'),
           ),
         ],
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
 
-      home:OtpPage()
+
+      home:PhoneNumber(),
+
 
 
     );
