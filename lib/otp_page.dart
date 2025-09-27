60e793c7c5eced820016ffec6fe7cf95f7b292ec
PriyadhaPrakash <priyadhaprakash76@gmail.com>
2025-09-27 12:06:21 +0530

    alignment

--- lib/otp_page.dart
+++ lib/otp_page.dart
@@ -16,37 +16,40 @@ class _OtpPageState extends State<OtpPage> {
       appBar: AppBar(
         backgroundColor: const Color(0xFF0F172A),
         leading: BackButton(),
-        title: Text("OTP Verification",style: TextStyle(color: const Color(0xFFFFFFFF) ),),
+        title: Text("Verification",style: TextStyle(color: const Color(0xFFFFFFFF) ),),
         centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Verification",
-              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 32,color: Color(0xFFFFFFFF) ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              "Enter the 4-digit OTP sent to your phone",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16,color: Color(0xFFFFFFFF) ),
-            ),
-            SizedBox(height: 40),
-            OtpTextField(
-              numberOfFields: 4,
-              borderColor: Colors.cyan,
-              showFieldAsBox: true,
-              onSubmit: (String verificationCode) {
-                print("Entered OTP: $verificationCode");
-                ScaffoldMessenger.of(
-                  context,
-                ).showSnackBar(SnackBar(content: Text("OTP Entered")));
-              },
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25,vertical: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                "Verification",
+                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 32,color: Color(0xFFFFFFFF) ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                "Enter the 4-digit OTP sent to your phone",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16,color: Color(0xFFFFFFFF) ),
+              ),
+              SizedBox(height: 40),
+              OtpTextField(
+                numberOfFields: 4,
+                borderColor: Colors.cyan,
+                showFieldAsBox: true,
+                onSubmit: (String verificationCode) {
+                  print("Entered OTP: $verificationCode");
+                  ScaffoldMessenger.of(
+                    context,
+                  ).showSnackBar(SnackBar(content: Text("OTP Entered")));
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
