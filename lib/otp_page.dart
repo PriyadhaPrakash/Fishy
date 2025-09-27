b536db079bee7dc6aee31e29f8bfe8cf3ecfd91f
PriyadhaPrakash <priyadhaprakash76@gmail.com>
2025-09-27 12:22:25 +0530

    otp edited

--- lib/otp_page.dart
+++ lib/otp_page.dart
@@ -16,25 +16,25 @@ class _OtpPageState extends State<OtpPage> {
       appBar: AppBar(
         backgroundColor: const Color(0xFF0F172A),
         leading: BackButton(),
-        title: Text("Verification",style: TextStyle(color: const Color(0xFFFFFFFF) ),),
+        title: Text("Verification",style: TextStyle(color: const Color(0xFFFFFFFF),fontSize: 18 ),),
         centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24.0),
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25,vertical: 20),
+          padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Text(
-                "Verification",
-                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 32,color: Color(0xFFFFFFFF) ),
+                "Enter Code",
+                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 28,color: Color(0xFFFFFFFF) ),
               ),
               SizedBox(height: 20),
               Text(
-                "Enter the 4-digit OTP sent to your phone",
+                "We sent a 4-digit code sent \n to your phone",
                 textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16,color: Color(0xFFFFFFFF) ),
+                style: TextStyle(fontSize: 16,color:Colors.grey ),
               ),
               SizedBox(height: 40),
               OtpTextField(
